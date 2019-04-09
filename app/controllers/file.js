0646681478fec28661b4ced29902644d8279fa1f
wodelu <1058744843@qq.com>
2019-04-09 09:59:20 +0800

    下载临时提交

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -1,12 +1,20 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default Controller.extend({
-
+	bmOss: service(),
 	actions: {
 		uploadFile() {
 			// var input = document.getElementById("file");
 			// var file = input.files[0];
 			// console.log("111"+file);
+		},
+		download(param) {
+			let accept = param.accept;
+			let uuid = param.uuid;
+			let client = this.bmOss.get('ossClient');
+			// let url = client.signatureUrl(accept+uuid);
+
 		}
 	},
 });
