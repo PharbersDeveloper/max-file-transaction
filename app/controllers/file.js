104fe7b44cb3003e4a06e0d51e3bb2c640b3ac43
wodelu <1058744843@qq.com>
2019-04-09 10:25:57 +0800

    去掉console

--- app/controllers/file.js
+++ app/controllers/file.js
@@ -4,21 +4,20 @@ import { inject as service } from '@ember/service';
 export default Controller.extend({
 	upload_service: service(),
 
-	
+
 	bmOss: service(),
 	actions: {
 		uploadFile(event) {
-			console.log(event);
 			let that = this,
 				file = event["target"].files[0],
 				formData = new FormData();
-				
+
 				formData.append('file', file);
 				window.console.log(file);
 			if(file.size > 0) {
 				that.upload_service.uploadFile(formData)
 				.then(res => {
-					window.console.log("upload result "+res)	
+					window.console.log("upload result "+res)
 				})
 				.catch(err => {
 					window.console.log("upload error "+err)
@@ -27,12 +26,7 @@ export default Controller.extend({
 				window.console.log("get file error")
 			}
 		},
-		
-		uploadFile() {
-			// var input = document.getElementById("file");
-			// var file = input.files[0];
-			// console.log("111"+file);
-		},
+
 		download(param) {
 			let accept = param.accept;
 			let uuid = param.uuid;
