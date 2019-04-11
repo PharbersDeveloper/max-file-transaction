9957cb9cd764304fa2aa83d2429bbc4f1eb462c1
yvettemuki <373628977@qq.com>
2019-04-11 14:44:38 +0800

    修改

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,34 +3,7 @@ import { inject as service } from '@ember/service';
 import EmberObject from '@ember/object';
 
 export default Controller.extend({
-	cookies: service(),
-	clientId: '5caeafd7d23dc2064f4891ba', // 5cac3206d23dc25e3bbb1ffe
-	clientSecret: '5c90db71eeefcc082c0823b2',
-	redirectUri: 'http://192.168.200.200:4200/oauth-callback',
-	beforeModel({ targetName }) {
-		let cookies = this.get('cookies'),
-			token = cookies.read('token');
-
-		if (!token && targetName !== 'oauth-callback') {
-			let host = 'http://192.168.100.116:31416',
-				version = 'v0',
-				resource = 'GenerateUserAgent',
-				scope = 'Pharbers',
-				url = '';
-
-			url = `?response_type=code
-                        &client_id=${this.get('clientId')}
-                        &client_secret=${this.get('clientSecret')}
-                        &scope=${scope}
-						&redirect_uri=${this.get('redirectUri')}`.
-				replace(/\n/gm, '').
-				replace(/ /gm, '').
-				replace(/\t/gm, '');
-			window.location = [host, version, resource, url].join('/');
-		} else {
-            this.transitionTo("file")
-        }
-	}
+	
     // login_service: service(),
     // token: service(),
     // toast: service(),
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,37 @@ export default Route.extend({
         // this.transitionTo('home'); // Implicitly aborts the on-going transition.
     // }
     // withLayout: false,
-    model() {
-        return RSVP.hash({
-            errorImg: 'https://bm-web.oss-cn-beijing.aliyuncs.com/icon_status_error%402x.png'
-        });
-    },
+    // model() {
+    //     return RSVP.hash({
+    //         errorImg: 'https://bm-web.oss-cn-beijing.aliyuncs.com/icon_status_error%402x.png'
+    //     });
+	// },
+	cookies: service(),
+	clientId: '5caeafd7d23dc2064f4891ba', // 5cac3206d23dc25e3bbb1ffe
+	clientSecret: '5c90db71eeefcc082c0823b2',
+	redirectUri: 'http://192.168.200.200:4200/oauth-callback',
+	beforeModel({ targetName }) {
+		let cookies = this.get('cookies'),
+			token = cookies.read('token');
+
+		if (!token && targetName !== 'oauth-callback') {
+			let host = 'http://192.168.100.116:31416',
+				version = 'v0',
+				resource = 'GenerateUserAgent',
+				scope = 'Pharbers',
+				url = '';
+
+			url = `?response_type=code
+                        &client_id=${this.get('clientId')}
+                        &client_secret=${this.get('clientSecret')}
+                        &scope=${scope}
+						&redirect_uri=${this.get('redirectUri')}`.
+				replace(/\n/gm, '').
+				replace(/ /gm, '').
+				replace(/\t/gm, '');
+			window.location = [host, version, resource, url].join('/');
+		} else {
+            this.transitionTo("file")
+        }
+	}
 });
