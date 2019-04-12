27b0fe06ffe452d3614823531e09e10bd9608246
Chenghao Sun <601330984@qq.com>
2019-04-12 11:57:47 +0800

    简易oauthservice

---
+++ app/services/oauth-service.js
@@ -0,0 +1,94 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+
+export default Service.extend({
+    cookies: service(),
+    ajax: service(),
+
+    clientId: "5caeb116d23dc2064f4891bb",
+    clientSecret: '5c90db71eeefcc082c0823b2',
+    redirectUri: 'http://192.168.100.177:4200/oauth-callback',
+    scope: "Pharbers",
+
+    haveAuth: false,
+
+    oauthOperation() {
+        let cookies = this.get('cookies'),
+			token = cookies.read('token');
+
+		if (!token) {
+			let host = 'http://192.168.100.116:31416',
+				version = 'v0',
+				resource = 'GenerateUserAgent',
+				// scope = 'Pharbers',
+				url = '';
+
+			url = `?response_type=code
+                        &client_id=${this.get('clientId')}
+                        &client_secret=${this.get('clientSecret')}
+                        &scope=${this.get('scope')}
+						&redirect_uri=${this.get('redirectUri')}`.
+				replace(/\n/gm, '').
+				replace(/ /gm, '').
+				replace(/\t/gm, '');
+			window.location = [host, version, resource, url].join('/');
+		} else {
+            this.transitionTo("file")
+        }
+    },
+
+    oauthCallback(transition) {
+        window.console.log("in the oauth-callback");
+		let version = 'v0',
+			resource = 'GenerateAccessToken',
+			// scope = 'fileTrans',
+			url = '',
+			cookies = this.get('cookies');
+
+		const ajax = this.get('ajax'),
+			{ queryParams } = transition;
+
+		if (queryParams.code && queryParams.state) {
+			url = `?response_type=authorization_code
+					&client_id=${this.get('clientId')}
+					&client_secret=${this.get('clientSecret')}
+					&scope=${this.get('scope')}
+					&redirect_uri=${this.get('redirectUri')}
+					&code=${queryParams.code}
+					&state=${queryParams.state}`.
+				replace(/\n/gm, '').
+				replace(/ /gm, '').
+				replace(/\t/gm, '');
+			ajax.request([version, resource, url].join('/'))
+				.then(response => {
+                    let expiry = new Date(response.expiry);
+                    let options = {
+                        // domain: '.pharbers.com',
+                        // path: '/'
+                        expires: expiry
+                    }
+                    cookies.write('token', response.access_token, options);
+                    
+					cookies.write('account_id', response.account_id, options);
+					cookies.write('access_token', response.access_token, options);
+					cookies.write('refresh_token', response.refresh_token, options);
+                    cookies.write('token_type', response.token_type, options);
+					// cookies.write('expiry', response.expiry, options);
+                    this.set("haveAuth", true);
+					this.transitionTo('file');
+				});
+		} else {
+			this.transitionTo('file');
+		}
+    },
+
+    removeAuth() {
+        this.cookies.clear("token")
+        this.cookies.clear("account_id")
+        this.cookies.clear("access_token")
+        this.cookies.clear("refresh_token")
+        this.cookies.clear("token_type")
+        this.set("haveAuth", false);
+    }
+    
+});
